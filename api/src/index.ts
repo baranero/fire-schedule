0113f4eb8cae7efba4cded8581eefe673d054ce6
baranero <jakub.baran2@gmail.com>
2024-01-07 23:41:56 +0100

    connected backend with supabase

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,31 @@
-import express from 'express'
+import express from 'express';
+import supabase from './config/supabase';
 
-const app = express()
+async function checkSupabaseConnection() {
+    try {
+        let { data, error, status } = await supabase
+            .from('Firefighters') // Replace with your table name
+            .select('*')
+            .limit(1); // Get only one record
+
+        if (error && status !== 406) {
+            throw error;
+        }
+
+        if (data) {
+            console.log('Connection successful. Data:', data);
+        } else {
+            console.log('No data returned');
+        }
+    } catch (error) {
+        console.error('Error checking Supabase connection:', error);
+    }
+}
+
+checkSupabaseConnection();
+
+const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
-
-})
+});
